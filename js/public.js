41ea1bdbee8200b44ef51337a9e9b14c23938f2d
Your Name <you@example.com>
2015-12-24 11:24:54 +0800

    init repository

--- js/public.js
+++ js/public.js
@@ -5,3 +5,20 @@ var rem=desW/100;
 if(winW>desW)winW=desW;//如果是PC端，就不修改字体了
 //    winW=winW>desW?desW:winW;//也可以这么写
 document.documentElement.style.fontSize=winW/rem+"px";
+
+
+function orientationChange() {
+    switch(window.orientation) {
+        case 0:
+        default :
+            document.documentElement.style.transform("rotate(90deg)");
+    }
+}
+
+addEventListener('load', function(){
+
+    orientationChange();
+
+    window.onorientationchange = orientationChange;
+
+});
